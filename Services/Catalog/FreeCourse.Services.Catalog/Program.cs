cc09c0ea74012cd9cba2c51f6da1a4fc6dfd79e7
berkayemir <brky_emr92@windowslive.com>
2022-12-18 21:00:31 +0300

    CategoryService

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,7 @@
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(StartupBase));
+
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+
+builder.Configuration.GetSection("DatabaseSettings");
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<IDatabaseSettings>>().Value;
+});
 
 var app = builder.Build();
 
