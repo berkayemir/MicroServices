834cb3f71faf0a93ba36bfb54f6f910ad5f41b0b
berkayemir <brky_emr92@windowslive.com>
2023-01-30 22:13:18 +0300

    Migration

--- IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using FreeCourse.IdentityServer.Data;
+using FreeCourse.IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -45,15 +49,33 @@ namespace FreeCourse.IdentityServer
 
                 var host = CreateHostBuilder(args).Build();
 
-                if (seed)
+
+                using (var scope=host.Services.CreateScope())
                 {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
+                    var serviceProvider = scope.ServiceProvider;
+
+                    var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+                    applicationDbContext.Database.Migrate();
+
+                    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+                    if (!userManager.Users.Any())
+                    {
+                        userManager.CreateAsync(new ApplicationUser { UserName = "bemir", Email = "b-emir@gmail.com", City = "İstanbul" }, "Berkay1992*").Wait();
+                    }
                 }
+
+
+
+
+
+
+
+
+
+
+
 
                 Log.Information("Starting host...");
                 host.Run();
